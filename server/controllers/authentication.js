9ecc308de7e61cc4839e50a7a5d9bdd7f4fc360e
Anson Felder <afelder09@gmail.com>
2018-11-16 15:29:05 -0500

    Added signup endpoint and started authenitcation flow

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,30 @@
+const User = require('../models/user');
+
 exports.signup = (req, res, next) => {
-    res.send({ success: 'true' });
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(422).send({ error: 'You must provide an email and a password'})
+    }
+    // See if a user with the given email exists
+    User.findOne({ email }, (err, existingUser) => {
+        if (err) { return next(err); }
+
+        // If a user with email does exists, return an error
+        if (existingUser) {
+            return res.status(422).send({ error: 'Email is in use'});
+        }
+
+        // If a user with email does not exist, create and save user  record
+        const user = new User({ email, password })
+        user.save((err) => {
+            if (err) {return next(err)}
+            res.json({ success: true })
+        });
+    });
+
+
+
+    // Respond to request indicating the user was created
+
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
+const bcrypt = require('bcrypt-nodejs');
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 //Define model
 const userSchema = new Schema({
     email: {
@@ -9,6 +10,29 @@ const userSchema = new Schema({
         lowercase: true
     },
     password: String
+})
+ 
+// On Save Hook, encrypt password
+// Before saving a model, run this function
+userSchema.pre('save', (next) => {
+    // get access to the user model
+    const user = this;
+    console.log('User: ', user);
+    // generate a salt
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {return next(err)}
+        console.log('Salt: ', salt);
+        // hash (encrypt) our password using the salt
+        bcrypt.hash(user.password, salt, null, (err,hash) => {
+            if (err) {return next(err)}
+            // Overwrite plain text password with encrypted password
+            console.log('Hash', hash);
+            console.log('Saving encrypted password')
+            user.password = hash;
+            console.log(user);
+            next();
+        })
+    })
 })
 
 
